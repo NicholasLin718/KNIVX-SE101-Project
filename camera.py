f610ad923d0fd465e4ed346c79759593228ee59a
i6hu <i6hu@uwaterloo.ca>
2021-11-23 16:18:56 -0500

    integration

--- camera.py
+++ camera.py
@@ -1,15 +1,15 @@
 from picamera import PiCamera
 from time import sleep
-camera=PiCamera()
-#rotate picture
-camera.rotation=180
-#camera resolution
-camera.resolution=(200, 200)
-#take picture
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/test1.jpg')
-camera.stop_preview()
-print("Picture taken")
-#save picture to local computer 
-
+def takePicture():
+    camera=PiCamera()
+    #rotate picture
+    camera.rotation=180
+    #camera resolution
+    camera.resolution=(200, 200)
+    #take picture
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/test1.jpg')
+    camera.stop_preview()
+    print("Picture taken")
+    #save picture to local computer 
